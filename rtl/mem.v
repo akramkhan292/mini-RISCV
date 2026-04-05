67d01a7da04c8e9a0c352ba4f58932aa22791072
akram khan <akramkhan75533@gmail.com>
2026-04-05 15:01:06 +0530

    added program interface for writing program into instruction memory

--- rtl/mem.v
+++ rtl/mem.v
@@ -1,13 +1,18 @@
 module inst_mem(
+    input wire clk,
     input wire [31:0] addr,
     output wire [31:0] instruction
+    // Add programing Interface
+    input wire [31:0] prog_addr,
+    input wire [31:0] prog_data,
+    input wire        prog_we
 );
     reg [31:0] mem [255:0];
 
-    initial begin
-        $readmemh("program.mem", mem);
+    always @(posedge clk) begin
+        if(prog_we)
+            mem[prog_addr] <= prog_data;
     end
-
     // addr[9:2] converts byte address to word index
     assign instruction = mem[addr[9:2]];
 
